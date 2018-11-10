1019c9e3849aa062d84489dea259285847e24836
Liam Jenkins <jenkilia000@stu.asd103.org>
2018-11-09 18:13:58 -0800

    This commit adds a Utility class for the collector subsystem and changes code in the TeleOp to use it.
    Signed-off-by: Liam Jenkins <jenkilia000@stu.asd103.org>
    
    Fixes: #4

--- OpModes/RR2Teleop.java
+++ OpModes/RR2Teleop.java
@@ -12,55 +12,72 @@ import org.firstinspires.ftc.teamcode.Utilities.*;
 
 @TeleOp(name = "RR2Teleop(Java)", group = "")
 public class RR2Teleop extends OpMode {
-
+  //Create motor objects
   private DcMotor leftF;
   private DcMotor rightF;
   private DcMotor leftB;
   private DcMotor rightB;
+  private DcMotor body;
+  private DcMotor slide;
+  private DcMotor spinner;
   private DcMotor lift;
+  //Create subsection objects
   private MecanumChassis chassis;
-  private AndroidTextToSpeech androidTextToSpeech;
-  
+  private Collector collector;
+  //Add necessary variables
   private double forward;
   private double clockwise;
   private double right;
+  private double bodyPower;
+  private double slidePower;
+  private double spinnerPower;
 
   /**
    * This function is executed when this Op Mode is selected from the Driver Station.
    */
   @Override
   public void init() {
+    //Map motor objects to config profile
     leftF = hardwareMap.dcMotor.get("leftF");
     rightF = hardwareMap.dcMotor.get("rightF");
     leftB = hardwareMap.dcMotor.get("leftB");
     rightB = hardwareMap.dcMotor.get("rightB");
-    chassis = new MecanumChassis(leftF, rightF, leftB, rightB);
+    body = hardwareMap.dcMotor.get("body");
+    slide = hardwareMap.dcMotor.get("slide");
+    spinner = hardwareMap.dcMotor.get("spinner");
     lift = hardwareMap.dcMotor.get("lift");
-    androidTextToSpeech = new AndroidTextToSpeech();
-    // Put initialization blocks here.
-    androidTextToSpeech.initialize();
-    androidTextToSpeech.setLanguageAndCountry("en", "US");
+    //Set up the subsection objects
+    chassis = new MecanumChassis(leftF, rightF, leftB, rightB);
+    collector = new Collector(body, slide, spinner);
+    //Make necessary motor mode adjustments
     lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
   }
-
+  
+  /**
+   * This function is executed when the driver presses the play button.
+   */
   @Override
   public void loop() {
     // Put loop blocks here.
     getInput();
     chassis.driveMecanum(forward, clockwise, right);
+    collector.driveCollector(bodyPower, slidePower, spinnerPower);
     moveLift();
     telemetry.addData("Angle:", lift.getCurrentPosition());
     telemetry.update();
   }
 
   /**
-   * Describe this function...
+   * This function takes the inputs from the Gamepads and converts them to variables.
    */
   private void getInput() {
     forward = -((gamepad1.left_stick_y + gamepad1.right_stick_y) / 2);
     right = gamepad1.right_stick_x;
     clockwise = -((gamepad1.left_stick_y - gamepad1.right_stick_y) / 2);
+    bodyPower = gamepad2.left_trigger*(-gamepad2.right_trigger);
+    slidePower = gamepad2.left_stick_y;
+    spinnerPower = gamepad2.right_stick_y;
   }
 
   /**
@@ -68,23 +85,5 @@ public class RR2Teleop extends OpMode {
    */
   private void moveLift() {
     lift.setPower((-gamepad1.left_trigger)+(gamepad1.right_trigger));
-  }
-
-  /**
-   * Describe this function...
-   */
-  private void moveLatch() {
-  }
-
-  /**
-   * Describe this function...
-   */
-  private void intake() {
-  }
-
-  /**
-   * Describe this function...
-   */
-  private void deposit() {
   }
 }
---
+++ Utilities/Collector.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.Utilities;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+public class Collector {
+    DcMotor body;
+    DcMotor slide;
+    DcMotor spinner;
+    
+    public Collector(DcMotor bodyInput, DcMotor slideInput, DcMotor spinnerInput) {
+        body = bodyInput;
+        slide = slideInput;
+        spinner = spinnerInput;
+    }
+    
+    public void driveCollector(double bodyPower, double slidePower, double collectPower) {
+        body.setPower(bodyPower);
+        slide.setPower(slidePower);
+        spinner.setPower(collectPower);
+        
+    }
+    
+    public void controlCollector(int bodyAngle, int slideDistance, boolean collect) {
+        
+    }
+}
